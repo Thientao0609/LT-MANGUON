e3d714d0f054897cef8fd134277fa0753e586ed0
nguyen thanh thien <nguyenthanhthien0609@gmail.com>
2025-04-13 20:14:27 +0700

    update

--- buoi5/demoST23A/controllers/home_controller.js
+++ buoi5/demoST23A/controllers/home_controller.js
@@ -1,3 +1,7 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const salt = bcrypt.genSaltSync(10);
+
 class HomeController {
   static index(req, res) {
     // let q = req.query.q;
@@ -8,6 +12,38 @@ class HomeController {
     console.log(params);
 
     res.render("index");
+  }
+
+  static login(req, res) {
+    console.log("login");
+    res.render("login");
+  }
+
+  static resgiter(req, res) {
+    res.render("resgiter");
+  }
+  static async newresgiter(req, res) {
+    let { email, password } = req.body;
+    password = bcrypt.hashSync(password, salt);
+    let user = await User.create({ email, password });
+    res.render("resgiter");
+  }
+
+  static async createlogin(req, res) {
+    console.log(req.body);
+    let { email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (user) {
+      let check = bcrypt.compareSync(password, user.password);
+      if (check) {
+        req.session.user = user;
+        res.redirect("/");
+      } else {
+        res.render("login");
+      }
+    } else {
+      res.render("login");
+    }
   }
 }
 
